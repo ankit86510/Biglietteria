6fdf57d1444d64b825157a9be31a65e58f265c61
Ankit KUMAR <ankit.kumar@studenti.unipr.it>
2021-12-06 23:45:35 +0100

    final

--- Client/LogIn.cs
+++ Client/LogIn.cs
@@ -21,11 +21,6 @@ namespace Client
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             try

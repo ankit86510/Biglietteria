8246cefd03018d5bfdb2b3825765b064ebd14c59
Omar-coder-ai <geekandgraphics@outlook.com>
2021-12-08 20:24:29 +0100

    Logo finestra Registrazione

--- Client/Registrazione.Designer.cs
+++ Client/Registrazione.Designer.cs
@@ -29,6 +29,7 @@ namespace Client
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registrazione));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -47,9 +48,10 @@ namespace Client
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(132, 43);
+            this.label1.Location = new System.Drawing.Point(176, 53);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 34);
+            this.label1.Size = new System.Drawing.Size(81, 41);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome";
             // 
@@ -57,9 +59,10 @@ namespace Client
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(132, 99);
+            this.label2.Location = new System.Drawing.Point(176, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 34);
+            this.label2.Size = new System.Drawing.Size(118, 41);
             this.label2.TabIndex = 1;
             this.label2.Text = "Cognome";
             // 
@@ -67,9 +70,10 @@ namespace Client
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(132, 150);
+            this.label3.Location = new System.Drawing.Point(176, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 34);
+            this.label3.Size = new System.Drawing.Size(77, 41);
             this.label3.TabIndex = 2;
             this.label3.Text = "Email";
             // 
@@ -77,9 +81,10 @@ namespace Client
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(132, 207);
+            this.label4.Location = new System.Drawing.Point(176, 255);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(93, 34);
+            this.label4.Size = new System.Drawing.Size(115, 41);
             this.label4.TabIndex = 3;
             this.label4.Text = "Password";
             // 
@@ -87,47 +92,53 @@ namespace Client
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(132, 260);
+            this.label5.Location = new System.Drawing.Point(176, 320);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(135, 34);
+            this.label5.Size = new System.Drawing.Size(166, 41);
             this.label5.TabIndex = 4;
             this.label5.Text = "Data di nascita";
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(367, 51);
+            this.txtNome.Location = new System.Drawing.Point(489, 63);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(238, 20);
+            this.txtNome.Size = new System.Drawing.Size(316, 22);
             this.txtNome.TabIndex = 5;
             // 
             // txtCognome
             // 
-            this.txtCognome.Location = new System.Drawing.Point(367, 107);
+            this.txtCognome.Location = new System.Drawing.Point(489, 132);
+            this.txtCognome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCognome.Name = "txtCognome";
-            this.txtCognome.Size = new System.Drawing.Size(238, 20);
+            this.txtCognome.Size = new System.Drawing.Size(316, 22);
             this.txtCognome.TabIndex = 6;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(367, 158);
+            this.txtEmail.Location = new System.Drawing.Point(489, 194);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(238, 20);
+            this.txtEmail.Size = new System.Drawing.Size(316, 22);
             this.txtEmail.TabIndex = 7;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(367, 215);
+            this.txtPassword.Location = new System.Drawing.Point(489, 265);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(238, 20);
+            this.txtPassword.Size = new System.Drawing.Size(316, 22);
             this.txtPassword.TabIndex = 8;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(283, 329);
+            this.button1.Location = new System.Drawing.Point(377, 405);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(214, 39);
+            this.button1.Size = new System.Drawing.Size(285, 48);
             this.button1.TabIndex = 10;
             this.button1.Text = "Registrati ora";
             this.button1.UseVisualStyleBackColor = true;
@@ -136,9 +147,10 @@ namespace Client
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(283, 385);
+            this.button2.Location = new System.Drawing.Point(377, 474);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(214, 44);
+            this.button2.Size = new System.Drawing.Size(285, 54);
             this.button2.TabIndex = 11;
             this.button2.Text = "Utente registrato? Clicca qui";
             this.button2.UseVisualStyleBackColor = true;
@@ -149,18 +161,19 @@ namespace Client
             this.dateTimePicker1.CustomFormat = "dd-MM-yyyy";
             this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(367, 260);
+            this.dateTimePicker1.Location = new System.Drawing.Point(489, 320);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTimePicker1.MaxDate = new System.DateTime(2021, 12, 7, 0, 0, 0, 0);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(238, 35);
+            this.dateTimePicker1.Size = new System.Drawing.Size(316, 41);
             this.dateTimePicker1.TabIndex = 12;
             this.dateTimePicker1.Value = new System.DateTime(2021, 12, 7, 0, 0, 0, 0);
             // 
             // Registrazione
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -173,6 +186,8 @@ namespace Client
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Registrazione";
             this.Text = "Registrazione";
             this.ResumeLayout(false);
